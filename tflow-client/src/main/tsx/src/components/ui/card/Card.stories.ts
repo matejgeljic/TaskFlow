b87f7b31600b90a69b1f7a13a0c2cd3ddf6b44bd
mgeljic <matej.geljic@dccs.eu>
2024-11-21 10:52:31 +0100

    Add storybook

---
+++ tflow-client/src/main/tsx/src/components/ui/card/Card.stories.ts
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Card from './Card';
+
+const meta: Meta<typeof Card> = {
+  component: Card,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const DefaultCard: Story = {
+  args: {},
+};
